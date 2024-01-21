434c34532025582c6ce69efefc639539dc97b6db
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 20:07:19 +0000

    Add files via upload

---
+++ 5.3 Lists - Predict & Run/main.py
@@ -0,0 +1,15 @@
+# -----------------------
+# Title:
+# Date: 
+# -----------------------
+
+### Note: Add comments to each line of the sample code to show your predictions.
+
+# Enter sample code below:
+
+
+
+
+
+# Enter Sweets list below:
+
