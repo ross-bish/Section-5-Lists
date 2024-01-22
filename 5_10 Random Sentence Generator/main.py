58598f8fca3e771c19eaf8a9113c734f1b0ce99e
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:54:27 +0000

    Update main.py

--- 5_10 Random Sentence Generator/main.py
+++ 5_10 Random Sentence Generator/main.py
@@ -3,18 +3,7 @@
 # Date:
 # ----------------------
 
-import random
 
-
-articles = ["the", "a", "one", "some", "any"]
-
-nouns = ["teacher", "student", "principal", "library", "school"]
-
-verbs = ["taught", "learned", "read", "walked", "ran"]
-
-prepositions = ["to", "from", "over", "under", "on"]
-
-print(random.choice(articles), random.choice(nouns), random.choice(verbs), random.choice(articles), random.choice(nouns), random.choice(prepositions), random.choice(verbs))
 
 
 
